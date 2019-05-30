6f320441c49b15c51ca9b6b70a96ce95905c6fde
Caio Vinicius Canic Silva <caiocanic@outlook.com>
2019-05-30 00:16:37 -0300

    Update the state matrix instead of recreating it

--- play_dino.py
+++ play_dino.py
@@ -7,36 +7,37 @@ POP_SIZE = 50
 P_CROSSOVER = 0.8
 P_MUTATION = 0.1
 N_GENERATIONS = 100
+N_BEST = 5
+N_GAMES_PLAYED = 10
 
+#TODO Try other values as fitness: min, mean+std
 def calc_fitness(subject, game):
     scores = []
-    for _ in range(10):
+    state_matrix = np.zeros((3, 30))
+    for _ in range(N_GAMES_PLAYED):
         game.reset()
         while not game.game_over:
-            state_matrix = create_state_matrix(game)
+            state_matrix = update_state_matrix(game, state_matrix)
             action = np.argmax(subject @ state_matrix.T)
             game.step(action)
         scores.append(game.get_score())
     return np.mean(scores)
 
-def create_state_matrix(game):
-    state = game.get_state()
-    state_matrix = np.zeros((3, 30))
-    a = 0
-    b = 10 
-    for i in range(3):
-        state_matrix[i, a:b] = state
-        a = b
-        b = b+b
-    return state_matrix
-    
 def play_game(best_individual):
     game = DinoGame(fps=60)
+    state_matrix = np.zeros((3, 30))
     while not game.game_over:
-        state_matrix = create_state_matrix(game)
+        state_matrix = update_state_matrix(game, state_matrix)
         action = np.argmax(best_individual @ state_matrix.T)
         game.step(action)
     return game.get_score()
+
+def update_state_matrix(game, state_matrix):
+    state = game.get_state()
+    state_matrix[0, 0:10] = state
+    state_matrix[1, 10:20] = state
+    state_matrix[2, 20:30] = state
+    return state_matrix
        
 game = DinoGame(fps=1000000)
 ga = Genetic(POP_SIZE, 30)
@@ -62,7 +63,7 @@ for _ in range(N_GENERATIONS):
     #   Roulette
     idx_roulette = se.roulette(fitness)
     #   N-best
-    idx_n_best = se.n_best(fitness, 5)
+    idx_n_best = se.n_best(fitness, N_BEST)
     idx_selected = np.concatenate((idx_roulette, idx_n_best))
     ga.set_population(total_population[idx_selected])
     ga.set_fitness(fitness[idx_selected])
